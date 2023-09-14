1d7e338429fbe2979539c484e25941535b436c3a
pyr0Hax <jvanzyl5@outlook.com>
2023-09-14 22:08:44 +0200

    modified script

--- bbs_server.py
+++ bbs_server.py
@@ -3,10 +3,13 @@ import threading
 import time
 import sqlite3
 import datetime
+import subprocess
+import os
 
-MAX_CHARS_PER_SECOND = 5
 DB_FILE = 'bbs.db'
 LOG_FILE = 'log.txt'
+online_users = 0
+unread_messages = 0
 
 def log_activity(message):
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -43,6 +46,16 @@ def create_database():
             sender TEXT NOT NULL,
             recipient TEXT NOT NULL,
             message TEXT NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS ForumMessages (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            subject TEXT NOT NULL,
+            message TEXT NOT NULL,
+            post_date DATETIME DEFAULT CURRENT_TIMESTAMP
         )
     ''')
 
@@ -136,8 +149,6 @@ def get_private_messages(username):
 
     return messages_received
 
-
-
 def send_private_message(sender, recipient, message):
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
@@ -172,13 +183,12 @@ def read_characters(client_socket):
         try:
             char_utf8 = char.decode('utf-8')
             data += char_utf8
-            client_socket.send(char)
+            client_socket.send(char)  # Send the character back to the client
         except UnicodeDecodeError:
             char_fallback = char.decode('latin-1')
             data += char_fallback
-            client_socket.send(char)
+            client_socket.send(char)  # Send the character back to the client
 
-        time.sleep(1 / MAX_CHARS_PER_SECOND)
     return data
 
 def show_address_book(client_socket):
@@ -192,15 +202,13 @@ def show_address_book(client_socket):
 
 def inbox(client_socket, username):
     messages_received = get_private_messages(username)
-    print(username)
     client_socket.send(b"\r\nInbox:\r\n")
     client_socket.send(b"\r\nReceived Messages:\r\n")
     if messages_received:
-        for sender, message in messages_received:
-            client_socket.send(f"From {sender}: {message}\r\n".encode('utf-8'))
+        for message_id, sender, _, message in messages_received:
+            client_socket.send(f"ID: {message_id}, From {sender}: {message}\r\n".encode('utf-8'))
     else:
         client_socket.send(b"No received messages.\r\n")
-    
 
 def send_message_menu(client_socket, authenticated_user):
     client_socket.send(b"\r\nEnter the recipient's username: ")
@@ -213,49 +221,6 @@ def send_message_menu(client_socket, authenticated_user):
 
     send_private_message(authenticated_user, recipient.strip(), formatted_message)
     client_socket.send(b"\r\nMessage sent successfully.\r\n")
-
-
-def register_client(client_socket):
-    client_socket.send(b"\r\nWelcome to My BBS!\r\n")
-    log_activity(f"Connection established from {client_socket.getpeername()}")
-
-    while True:
-        client_socket.send(b"\r\n1. Register\r\n2. Login\r\nEnter your choice: ")
-        choice = read_characters(client_socket)
-        
-        # Log client choice
-        log_activity(f"Client {client_socket.getpeername()} selected option {choice.strip()}")
-
-        if choice.strip() == '1':
-            client_socket.send(b"\r\nEnter a new username: ")
-            username = read_characters(client_socket)
-            client_socket.send(b"\r\nEnter a password: ")
-            password = read_characters(client_socket)
-            client_socket.send(b"\r\nEnter your email address: ")
-            email = read_characters(client_socket)
-            client_socket.send(b"\r\nEnter your full name: ")
-            full_name = read_characters(client_socket)
-            client_socket.send(b"\r\nEnter your BBS number (optional): ")
-            bbs_number = read_characters(client_socket)
-
-            if register_user(username, password, email, full_name, bbs_number=None):
-                client_socket.send(b"\r\nRegistration successful. You can now login.\r\n")
-                log_activity(f"User {username} registered.")
-            else:
-                client_socket.send(b"\r\nUsername already exists. Try a different one.\r\n")
-        elif choice.strip() == '2':
-            client_socket.send(b"\r\nEnter your username: ")
-            username = read_characters(client_socket)
-            client_socket.send(b"\r\nEnter your password: ")
-            password = read_characters(client_socket)
-            if authenticate_user(username, password):
-                client_socket.send(b"\r\nLogin successful!\r\n")
-                log_activity(f"User {username} logged in.")
-                return username
-            else:
-                client_socket.send(b"\r\nInvalid username or password. Please try again.\r\n")
-        else:
-            client_socket.send(b"\r\nInvalid choice. Please try again.\r\n")
 
 def view_profile(client_socket, authenticated_user):
     conn = sqlite3.connect(DB_FILE)
@@ -271,17 +236,61 @@ def view_profile(client_socket, authenticated_user):
         client_socket.send(f"Email: {user_profile[3]}\r\n".encode('utf-8'))
         client_socket.send(f"Full Name: {user_profile[4]}\r\n".encode('utf-8'))            
 
+def create_forum_table():
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS ForumMessages (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL,
+            subject TEXT NOT NULL,
+            message TEXT NOT NULL,
+            post_date DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+create_forum_table()
+
+def get_forum_messages():
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT id, subject FROM ForumMessages ORDER BY id DESC')
+    messages = cursor.fetchall()
+    conn.close()
+
+    return messages
+
+def post_forum_message(username, subject, message):
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+
+    cursor.execute('INSERT INTO ForumMessages (username, subject, message) VALUES (?, ?, ?)',
+                   (username, subject, message))
+    conn.commit()
+    conn.close()
+
 def handle_client(client_socket):
+    global online_users
+    global unread_messages
+
     authenticated_user = register_client(client_socket)
+    if authenticated_user:
+        online_users += 1
 
     main_menu = (
-        b"\r\n1. View Address Book\r\n2. View Registered Users\r\n3. View Profile\r\n4. Logout\r\nEnter your choice: "
+        f"\r\nOnline Users: {online_users}\r\n"
+        f"Unread Messages: {unread_messages}\r\n"
+        "\r\n1. View Address Book\r\n2. View Registered Users\r\n3. View Profile\r\n4. Forum\r\n5. Logout\r\n\r\nEnter your choice: "
     )
 
     profile_menu = (
     b"\r\n1. Inbox\r\n2. Show User Details\r\n3. Go Back to Main Menu\r\n4. Logout\r\nEnter your choice: "
 )
-
 
     address_book_menu = (
         b"\r\n1. Show Address Book\r\n2. Add BBS to Address Book\r\n3. Delete from Address Book\r\n4. Back to Profile Menu\r\nEnter your choice: "
@@ -365,44 +374,135 @@ def handle_client(client_socket):
                                 client_socket.send(f"Full Name: {full_name}\r\n".encode('utf-8'))
                                 if bbs_number:
                                     client_socket.send(f"BBS Number: {bbs_number}\r\n".encode('utf-8'))
-                                else:
-                                    client_socket.send(b"BBS Number: Not provided\r\n")
-                            else:
-                                client_socket.send(b"User details not found.\r\n")
+                            client_socket.send(b"\r\n")
                         elif profile_choice.strip() == '3':
                             break
                         elif profile_choice.strip() == '4':
-                            client_socket.send(b"\r\nLogged out successfully.\r\n")
+                            client_socket.send(b"\r\nLogging out...\r\n")
                             log_activity(f"{authenticated_user} logged out.")
-                            return
+                            break
                         else:
                             client_socket.send(b"\r\nInvalid choice. Please try again.\r\n")
-                elif choice.strip() == '4':
-                    client_socket.send(b"\r\nLogged out successfully.\r\n")
-                    log_activity(f"{authenticated_user} logged out.")
-                    authenticated_user = None
-                    break
 
+                elif choice.strip() == '4':
+                    while True:
+                        client_socket.send(b"\r\nForum:\r\n")
+                        client_socket.send(b"\r\n1. View Forum Topics\r\n2. Post a New Topic\r\n3. Back to Main Menu\r\nEnter your choice: ")
+                        forum_choice = read_characters(client_socket)
+
+                        if forum_choice.strip() == '1':
+                            client_socket.send(b"\r\nForum Topics:\r\n")
+                            forum_messages = get_forum_messages()
+                            if forum_messages:
+                                for index, (message_id, subject) in enumerate(forum_messages, start=1):
+                                    client_socket.send(f"{index}. {subject}\r\n".encode('utf-8'))
+                                client_socket.send(b"\r\n")
+                                client_socket.send(b"\r\nEnter the topic number to view or '0' to go back: ")
+                                topic_choice = read_characters(client_socket)
+                                topic_choice = topic_choice.strip()
+                                if topic_choice.isdigit():
+                                    topic_choice = int(topic_choice)
+                                    if 1 <= topic_choice <= len(forum_messages):
+                                        message_id = forum_messages[topic_choice - 1][0]
+                                        view_forum_message(client_socket, message_id)
+                                    elif topic_choice == 0:
+                                        continue
+                                    else:
+                                        client_socket.send(b"\r\nInvalid choice. Please try again.\r\n")
+                                else:
+                                    client_socket.send(b"\r\nInvalid choice. Please try again.\r\n")
+                            else:
+                                client_socket.send(b"\r\nNo topics available.\r\n")
+                        elif forum_choice.strip() == '2':
+                            client_socket.send(b"\r\nEnter the subject: ")
+                            subject = read_characters(client_socket)
+                            client_socket.send(b"\r\nEnter your message: ")
+                            message = read_characters(client_socket)
+                            post_forum_message(authenticated_user, subject.strip(), message.strip())
+                            client_socket.send(b"\r\nTopic posted successfully.\r\n")
+                        elif forum_choice.strip() == '3':
+                            break
+                        else:
+                            client_socket.send(b"\r\nInvalid choice. Please try again.\r\n")
+
+                elif choice.strip() == '5':
+                    client_socket.send(b"\r\nLogging out...\r\n")
+                    log_activity(f"{authenticated_user} logged out.")
+                    break
+                else:
+                    client_socket.send(b"\r\nInvalid choice. Please try again.\r\n")
         except Exception as e:
-            if not client_socket._closed:
-                error_message = f"Error: {str(e)}"
-                client_socket.send(b"\r\n" + error_message.encode() + b"\r\n")
-                log_activity(error_message)
+            log_activity(f"Error: {str(e)}")
             break
 
-    client_socket.close()
+def view_forum_message(client_socket, message_id):
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT username, subject, message, post_date FROM ForumMessages WHERE id = ?', (message_id,))
+    message_data = cursor.fetchone()
+    conn.close()
+
+    if message_data:
+        username, subject, message, post_date = message_data
+        client_socket.send(b"\r\nForum Message:\r\n")
+        client_socket.send(f"Username: {username}\r\n".encode('utf-8'))
+        client_socket.send(f"Subject: {subject}\r\n".encode('utf-8'))
+        client_socket.send(f"Date: {post_date}\r\n".encode('utf-8'))
+        client_socket.send(f"Message:\r\n{message}\r\n".encode('utf-8'))
+    else:
+        client_socket.send(b"\r\nForum message not found.\r\n")
+
+def register_client(client_socket):
+    client_socket.send(b"\r\nWelcome to the Bulletin Board System!\r\n")
+    while True:
+        client_socket.send(b"\r\n1. Register\r\n2. Login\r\n3. Quit\r\nEnter your choice: ")
+        choice = read_characters(client_socket)
+
+        if choice.strip() == '1':
+            client_socket.send(b"\r\nRegistration:\r\n")
+            client_socket.send(b"\r\nEnter a username: ")
+            username = read_characters(client_socket)
+            client_socket.send(b"\r\nEnter a password: ")
+            password = read_characters(client_socket)
+            client_socket.send(b"\r\nEnter your email: ")
+            email = read_characters(client_socket)
+            client_socket.send(b"\r\nEnter your full name: ")
+            full_name = read_characters(client_socket)
+            if register_user(username.strip(), password.strip(), email.strip(), full_name.strip()):
+                client_socket.send(b"\r\nRegistration successful. You can now log in.\r\n")
+                log_activity(f"New user registered: {username.strip()}")
+            else:
+                client_socket.send(b"\r\nRegistration failed. The username is already taken.\r\n")
+        elif choice.strip() == '2':
+            client_socket.send(b"\r\nLogin:\r\n")
+            client_socket.send(b"\r\nEnter your username: ")
+            username = read_characters(client_socket)
+            client_socket.send(b"\r\nEnter your password: ")
+            password = read_characters(client_socket)
+            if authenticate_user(username.strip(), password.strip()):
+                client_socket.send(b"\r\nLogin successful. Welcome, " + username.strip().encode('utf-8') + b"!\r\n")
+                log_activity(f"{username.strip()} logged in.")
+                return username.strip()
+            else:
+                client_socket.send(b"\r\nLogin failed. Invalid username or password.\r\n")
+        elif choice.strip() == '3':
+            client_socket.send(b"\r\nGoodbye!\r\n")
+            client_socket.close()
+            return None
+        else:
+            client_socket.send(b"\r\nInvalid choice. Please try again.\r\n")
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(('0.0.0.0', 12345))
     server.listen(5)
-
-    log_activity("Server started.")
+    print("Server is listening on port 12345...")
 
     while True:
-        client, addr = server.accept()
-        log_activity(f"Accepted connection from {addr[0]}:{addr[1]}")
-        client_handler = threading.Thread(target=handle_client, args=(client,))
+        client_socket, addr = server.accept()
+        print(f"Accepted connection from {addr[0]}:{addr[1]}")
+        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
         client_handler.start()
 
 if __name__ == "__main__":
